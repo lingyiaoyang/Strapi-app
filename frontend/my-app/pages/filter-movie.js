423aba951fa09c1d19a0d15ca483434fac77dd0c
lingyiaoyang <76722905+lingyiaoyang@users.noreply.github.com>
2021-05-22 22:58:15 +0800

    update

--- frontend/my-app/pages/filter-movie.js
+++ frontend/my-app/pages/filter-movie.js
@@ -1,29 +1,65 @@
-import React from 'react';
-import { getServerSideProps } from './movies/[genre]/[slug]';
-import { Box, Flex } from 'reflexbox';
+// import React from 'react';
+// // import { getServerSideProps } from './movies/[genre]/[slug]';
+// import { Box, Flex } from 'reflexbox';
 
-function FilterMovie({ movies }) {
+// function FilterMovie({ movies }) {
+//   return (
+//     <>
+//       <div>
+//         {movies.map((movies) => {
+//           return (
+//             <Box key={movies.id}>
+//               <h4>{movies.Movie}</h4> -
+//               {/* {movies.genre ? movies.genre.title : null} */}
+//               <br />
+//               {movies}
+//             </Box>
+//           );
+//         })}
+//       </div>
+//     </>
+//   );
+// }
+
+// export async function getServerSideProps() {
+//   const { API_URL } = process.env;
+
+//   const res = await fetch(`${API_URL}/movies`);
+//   const data = await res.json();
+
+//   return {
+//     props: {
+//       movies: data,
+//     },
+//   };
+// }
+
+// export default FilterMovie;
+
+import React from 'react';
+
+function filter_movie({ movies }) {
   return (
-    <>
-      <div>
-        {movies.map((movies) => {
-          return (
-            <Box key={movies.id}>
-              <h4>{movies.Movie}</h4> -
-              {movies.genre ? movies.genre.title : null}
-              <br />
-              {movie}
-            </Box>
-          );
-        })}
-      </div>
-    </>
+    <div>
+      {movies.map((movies) => {
+        return (
+          <div
+            key={movies.id}
+            css={{ margin: '8px', border: '3px solid black', width: '30%' }}
+          >
+            <h3>{movies.Movie}</h3>
+            <p>
+              {movies.Description} - {movies.Genre}
+            </p>
+          </div>
+        );
+      })}
+    </div>
   );
 }
 
 export async function getServerSideProps() {
-  const { API_URI } = process.env;
-
+  const { API_URL } = process.env;
   const res = await fetch(`${API_URL}/movies`);
   const data = await res.json();
 
@@ -34,4 +70,4 @@ export async function getServerSideProps() {
   };
 }
 
-export default FilterMovie;
+export default filter_movie;
