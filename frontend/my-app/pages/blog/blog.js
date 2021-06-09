3b00b4b08b99152c200ef08846e6198ab4a68fc1
lingyiaoyang <76722905+lingyiaoyang@users.noreply.github.com>
2021-06-09 18:24:10 +0800

    nothing

--- frontend/my-app/pages/blog/blog.js
+++ frontend/my-app/pages/blog/blog.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { css } from '@emotion/react';
 function blog() {
-	return (
-		<div
-			css={(theme) => ({
-				color: theme.colors.primary,
-				// background: theme.background.bg,
-			})}
-		>
-			test
-		</div>
-	);
+  return (
+    <div
+      css={(theme) => ({
+        color: theme.colors.primary,
+        // background: theme.background.bg,
+      })}
+    ></div>
+  );
 }
 
 export default blog;
